419071d43cbd8c3ff6e19cfebbc5f05e51c1f991
JaeYong-An <ax5388@naver.com>
2024-01-22 15:14:31 +0900

    thymeleaf

---
+++ _boot_thymeleaf/thymeleaf/src/main/java/com/example/thymeleaf/domain/SampleDTO.java
@@ -0,0 +1,16 @@
+package com.example.thymeleaf.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@NoArgsConstructor
+@Getter
+@ToString
+public class SampleDTO {
+
+	
+	private String p1, p2, p3;
+}
